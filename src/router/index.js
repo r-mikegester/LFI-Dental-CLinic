b2f8c7197d04fff392de5f12d10606bfd49827c4
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-19 20:11:53 +0800

    router: register admin pages to router

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import HomePage from "../views/HomePage.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    /* Static pages */
     {
       path: "/",
       name: "Home",
@@ -18,6 +19,67 @@ const router = createRouter({
       path: "/contact",
       name: "Contact Page",
       component: () => import("../views/ContactPage.vue"),
+    },
+    /* Admin-only pages */
+    {
+      path: "/admin",
+      name: "Admin Home Page",
+      component: () => import("../views/admin/HomePage.vue"),
+    },
+    {
+      path: "/admin/login",
+      name: "Admin Login Page",
+      component: () => import("../views/admin/LoginPage.vue"),
+    },
+    {
+      path: "/admin/schedules",
+      name: "Admin Schedules Page",
+      component: () => import("../views/admin/SchedulesPage.vue"),
+    },
+    {
+      path: "/admin/appointments",
+      name: "Admin Appointments Page",
+      component: () => import("../views/admin/AppointmentsPage.vue"),
+    },
+    {
+      path: "/admin/patient-records",
+      name: "Admin Patient Records Page",
+      component: () =>
+        import("../views/admin/patient-records/PatientRecordsPage.vue"),
+    },
+    {
+      path: "/admin/patient-records/:uid/medical-chart",
+      name: "Admin Medical Chart Page",
+      component: () =>
+        import("../views/admin/patient-records/MedicalChartPage.vue"),
+    },
+    {
+      path: "/admin/patient-records/:uid/dental-chart",
+      name: "Admin Dental Chart Page",
+      component: () =>
+        import("../views/admin/patient-records/DentalChartPage.vue"),
+    },
+    {
+      path: "/admin/patient-records/:uid/treatments",
+      name: "Admin Dental Treatments Page",
+      component: () =>
+        import("../views/admin/patient-records/DentalTreatmentsPage.vue"),
+    },
+    {
+      path: "/admin/patient-records/:uid/procedures",
+      name: "Admin Procedures Page",
+      component: () =>
+        import("../views/admin/patient-records/ProceduresPage.vue"),
+    },
+    {
+      path: "/admin/messages",
+      name: "Admin Messages Page",
+      component: () => import("../views/admin/MessagesPage.vue"),
+    },
+    {
+      path: "/admin/account-settings",
+      name: "Admin Account Settings Page",
+      component: () => import("../views/admin/AccountSettingsPage.vue"),
     },
   ],
 });
