45a7d1159d81a9f3c82314c64ae2da2cdd037829
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-21 00:30:31 +0800

    admin/BaseLayout.vue: add component for base layout of admin pages

---
+++ src/components/admin/BaseLayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Footer from "./PageFooter.vue";
+import SideBar from "./SideBar.vue";
+</script>
+
+<template>
+  <div class="grid grid-rows-[minmax(100vh,_1fr)_auto] min-h-screen">
+    <div class="grid grid-cols-[auto_1fr]">
+      <div>
+        <SideBar />
+      </div>
+      <main class="p-6 lg:p-12 text-sky-600 font-[Poppins]">
+        <slot></slot>
+      </main>
+    </div>
+    <Footer />
+  </div>
+</template>
