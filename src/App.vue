4dfe2eaa2449264e6bcf70afb5278dd333930b04
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-20 18:32:20 +0800

    App.vue: remove patient footer on root component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup>
 import { RouterView } from "vue-router";
-import NavBar from "./components/patients-view/NavBar.vue";
 </script>
 
 <template>
-  <header>
-    <NavBar />
-  </header>
-
   <RouterView />
 </template>
