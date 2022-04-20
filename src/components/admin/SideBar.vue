6116d8fdfa90d48c495c22ab6212218dd0867646
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-20 22:43:20 +0800

    SideBar.vue: create component for admin sidebar

---
+++ src/components/admin/SideBar.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav
+    class="w-64 bg-teal-500/40 text-sky-700 hidden md:grid grid-rows-[auto_1fr]"
+  >
+    <!-- image -->
+    <div class="flex flex-col items-center py-6">
+      <img src="../../assets/img/nav-person.png" class="h-16" />
+      <span>Admin User</span>
+    </div>
+    <!-- Nav items -->
+    <div class="flex flex-col">
+      <RouterLink
+        class="sidebar-item hover:bg-teal-500/40 py-3 px-8 flex items-center"
+        :to="{ name: 'Admin Home Page' }"
+      >
+        <img src="../../assets/img/nav-house.png" class="mr-5" />
+        <span>Home</span>
+      </RouterLink>
+      <RouterLink
+        class="sidebar-item hover:bg-teal-500/40 py-3 px-8 flex items-center"
+        :to="{ name: 'Admin Schedules Page' }"
+      >
+        <img src="../../assets/img/nav-calendar.png" class="mr-5" />
+        <span>Schedules</span>
+      </RouterLink>
+      <RouterLink
+        class="sidebar-item hover:bg-teal-500/40 py-3 px-8 flex items-center"
+        :to="{ name: 'Admin Appointments Page' }"
+      >
+        <img src="../../assets/img/nav-bell.png" class="mr-5" />
+        <span>Appointments</span>
+      </RouterLink>
+      <RouterLink
+        class="sidebar-item hover:bg-teal-500/40 py-3 px-8 flex items-center"
+        :to="{ name: 'Admin Patient Records Page' }"
+      >
+        <img src="../../assets/img/nav-clipboard.png" class="mr-5" />
+        <span>Patient Records</span>
+      </RouterLink>
+      <RouterLink
+        class="sidebar-item hover:bg-teal-500/40 py-3 px-8 flex items-center"
+        :to="{ name: 'Admin Messages Page' }"
+      >
+        <img src="../../assets/img/nav-mail.png" class="mr-5" />
+        <span>Messages</span>
+      </RouterLink>
+      <RouterLink
+        class="sidebar-item hover:bg-teal-500/40 py-3 px-8 flex items-center"
+        :to="{ name: 'Admin Account Settings Page' }"
+      >
+        <img src="../../assets/img/nav-gear.png" class="mr-5" />
+        <span>Account Settings</span>
+      </RouterLink>
+      <a
+        href="#"
+        class="sidebar-item hover:bg-teal-500/40 py-3 px-8 flex items-center mt-auto"
+      >
+        <img src="../../assets/img/nav-exit.png" class="mr-5" />
+        <span>Log out</span>
+      </a>
+    </div>
+  </nav>
+</template>
