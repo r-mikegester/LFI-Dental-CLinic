b802baabe008f620e6fd160add051ebb6d4e20d0
Ejay098 <eddie.castuera031@gmail.com>
2022-04-21 20:23:05 +0800

    accountsettingspage

--- src/views/admin/AccountSettingsPage.vue
+++ src/views/admin/AccountSettingsPage.vue
@@ -1,5 +1,43 @@
+<script setup>
+import BaseLayout from "../../components/admin/BaseLayout.vue";
+import { RouterLink } from "vue-router";
+</script>
 <template>
-  <div>
-    <h1 class="text-4xl text-center font-bold">Admin Account Settings page</h1>
-  </div>
+  <BaseLayout>
+    <div class="ml-20 mr-80">
+      <div
+        class="border-b-[3px] border-teal-500 pt-20 p-4 pb-1 text-2xl text-center md:text-left font-semibold"
+      >
+        <h1>Account Settings</h1>
+      </div>
+
+      <div
+        class="border-b-[1px] border-teal-500 p-4 grid grid-cols-[2fr_4fr_3fr]"
+      >
+        <div>Profile Picture</div>
+        <div></div>
+        <div class="text-teal-500 hover:text-teal-400 text-right">
+          <button>Edit</button>
+        </div>
+      </div>
+      <div
+        class="border-b-[1px] border-teal-500 p-4 grid grid-cols-[2fr_4fr_3fr]"
+      >
+        <div>Name</div>
+        <div>Arni Isidro</div>
+        <div class="text-teal-500 hover:text-teal-400 text-right">
+          <button>Edit</button>
+        </div>
+      </div>
+      <div
+        class="border-b-[3px] border-teal-500 p-4 grid grid-cols-[2fr_4fr_3fr]"
+      >
+        <div>Password</div>
+        <div>*****</div>
+        <div class="text-teal-500 hover:text-teal-400 text-right">
+          <button>Edit</button>
+        </div>
+      </div>
+    </div>
+  </BaseLayout>
 </template>
