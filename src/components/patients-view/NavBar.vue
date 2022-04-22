5c916b4055052e58a6c76ad6a551c3172045eea2
Mike Gester26 <mikegester.sabuga023@gmail.com>
2022-04-22 10:07:45 +0800

    navvvvvvbar added links

--- src/components/patients-view/NavBar.vue
+++ src/components/patients-view/NavBar.vue
@@ -1,7 +1,22 @@
 <template>
-  <nav>
-    <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
-    <RouterLink :to="{ name: 'About Page' }">About Us</RouterLink>
-    <RouterLink :to="{ name: 'Contact Page' }">Contact Us</RouterLink>
+  <nav class="fixed navbar navbar-expand-lg row">
+    <router-link tag="li" class="col" :to="{ name: 'Home' }">Home</router-link>
+        <router-link tag="li" class="col" :to="{ name: 'Appointments Page' }">Appointments</router-link>
+    <router-link tag="li" class="col" :to="{ name: 'About Page' }">About Us</router-link>
+    <router-link tag="li" class="col" :to="{ name: 'Contact Page' }">Contact Us</router-link>
   </nav>
+
+  <div>
+    
+  </div>
+  
 </template>
+
+<style>
+nav li:hover,
+nav li:active .col {
+  background-color: indianred;
+  cursor: pointer;
+}
+</style>
+  
