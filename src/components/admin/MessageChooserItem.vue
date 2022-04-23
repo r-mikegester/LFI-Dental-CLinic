40f7bf88b7f512194a67d1a3799d8ae48f28a8dd
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-23 13:04:43 +0800

    MessageChooserItem.vue: create component for message items

---
+++ src/components/admin/MessageChooserItem.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+  message: Object,
+});
+
+const emit = defineEmits(["itemClicked"]);
+</script>
+
+<template>
+  <div
+    class="flex justify-between items-center px-3 py-2 hover:bg-gray-200 transition"
+    :class="{ 'bg-gray-100': props.message.clicked }"
+    @click="emit('itemClicked', message.uid)"
+  >
+    <div>{{ message.senderName }}</div>
+    <div>
+      <button
+        class="px-3 py-1 border text-sm border-teal-500 hover:bg-teal-400 hover:text-white transition duration-200 rounded-3xl"
+      >
+        Delete
+      </button>
+    </div>
+  </div>
+</template>
