d4cd72178918fefa37855cdf9b4f29782b4b9176
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-23 15:45:19 +0800

    getAllMessages.js: change returned field name

--- src/composables/api/getAllMessages.js
+++ src/composables/api/getAllMessages.js
@@ -2,5 +2,5 @@ export default async () => {
   const link = "http://localhost:5000/messages";
   const response = await fetch(link);
   const data = await response.json();
-  return data.messages;
+  return data.payload;
 };
