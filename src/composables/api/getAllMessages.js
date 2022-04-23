9bfc437de4b332816afb1339e8253a1e3f08ba82
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-23 13:04:09 +0800

    getAllMessages.js: add composable for retrieving messages from backend

---
+++ src/composables/api/getAllMessages.js
@@ -0,0 +1,6 @@
+export default async () => {
+  const link = "http://localhost:5000/messages";
+  const response = await fetch(link);
+  const data = await response.json();
+  return data.messages;
+};
