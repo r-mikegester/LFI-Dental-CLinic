6a891987bea33efb20e51eb210b2779189141db0
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-24 16:31:08 +0800

    PatientRecordsPage.vue: fix icon URLs

--- src/views/admin/patient-records/PatientRecordsPage.vue
+++ src/views/admin/patient-records/PatientRecordsPage.vue
@@ -11,7 +11,7 @@ import { RouterLink } from "vue-router";
     <div>
       <label class="relative block float-right">
         <span class="absolute inset-y-0 right-2 flex items-center pl-2">
-          <img src="../../../assets/img/search-3-24.png" />
+          <img src="../../../assets/img/search-icon.png" />
         </span>
         <input
           class="placeholder:normal placeholder:text-slate-400 block bg-white w-80 border-2 border-sky-400/60 border-3px py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
@@ -46,7 +46,7 @@ import { RouterLink } from "vue-router";
         </div>
         <div>
           <button class="mr-5 float-right">
-            <img src="../../../assets/img/delete-24.png" />
+            <img src="../../../assets/img/trash-icon.png" />
           </button>
         </div>
       </div>
