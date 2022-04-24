7881146c98608a538fc92f181ef295e040c6bccd
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-24 23:13:06 +0800

    LoginPage.vue: enable login

--- src/views/admin/LoginPage.vue
+++ src/views/admin/LoginPage.vue
@@ -1,5 +1,48 @@
 <script setup>
+import { computed, reactive, ref } from "vue";
+import { useRouter } from "vue-router";
 import Footer from "../../components/admin/PageFooter.vue";
+import signIn from "../../composables/auth/signIn";
+import SimpleModalDialog from "../../components/admin/SimpleModalDialog.vue";
+
+const userCredentials = reactive({
+  email: "",
+  password: "",
+});
+
+const isLoginButtonDisabled = computed(() => {
+  if (userCredentials.email === "") return true;
+  if (userCredentials.password === "") return true;
+  if (userCredentials.password && userCredentials.password.length < 8)
+    return true;
+  return false;
+});
+
+const router = useRouter();
+const onLogin = async () => {
+  try {
+    if (!isLoginButtonDisabled.value) {
+      await signIn(userCredentials.email, userCredentials.password);
+      router.push({ name: "Admin Home Page" });
+    }
+  } catch (e) {
+    switch (e.code) {
+      case "auth/invalid-email":
+      case "auth/wrong-password":
+      case "auth/user-not-found":
+        errorDialogBody.value =
+          "You have entered an incorrect email or password.";
+        isErrorDialogVisible.value = true;
+        break;
+      default:
+        errorDialogBody.value = `An unknown error occured. Code: ${e.code}`;
+        isErrorDialogVisible.value = true;
+    }
+  }
+};
+
+const isErrorDialogVisible = ref(false);
+const errorDialogBody = ref("");
 </script>
 
 <template>
@@ -24,10 +67,11 @@ import Footer from "../../components/admin/PageFooter.vue";
           <!-- inputs -->
           <div class="max-w-fit mx-auto text-right">
             <div class="mb-10">
-              <label class="mr-4">Username:</label>
+              <label class="mr-4">Email:</label>
               <input
                 type="text"
                 class="border border-teal-500 py-2 px-3 rounded-2xl max-w-[18rem] min-w-0"
+                v-model="userCredentials.email"
               />
             </div>
             <div class="mb-10">
@@ -35,16 +79,23 @@ import Footer from "../../components/admin/PageFooter.vue";
               <input
                 type="password"
                 class="border border-teal-500 py-2 px-3 rounded-2xl max-w-[18rem] min-w-0"
+                v-model="userCredentials.password"
               />
             </div>
           </div>
           <!-- actions -->
           <div class="flex justify-between items-center">
-            <a href="#" class="text-sm hover:border-b hover:border-sky-600"
-              >Forgot Password?</a
-            >
+            <a href="#" class="text-sm hover:border-b hover:border-sky-600">
+              Forgot Password?
+            </a>
             <button
+              type="button"
               class="px-6 py-2 rounded-3xl bg-teal-500 hover:bg-teal-400 transition duration-200 text-white"
+              :class="{
+                'pointer-events-none': isLoginButtonDisabled,
+                'bg-emerald-200': isLoginButtonDisabled,
+              }"
+              @click="onLogin()"
             >
               Login
             </button>
@@ -54,4 +105,25 @@ import Footer from "../../components/admin/PageFooter.vue";
     </main>
     <Footer />
   </div>
+  <SimpleModalDialog v-if="isErrorDialogVisible">
+    <template #header>
+      <div class="text-2xl font-semibold">Login Failed</div>
+    </template>
+    <template #body>
+      <div class="text-lg mb-3">
+        {{ errorDialogBody }}
+      </div>
+    </template>
+    <template #actions>
+      <div class="flex justify-end">
+        <button
+          type="button"
+          class="px-4 py-1 bg-teal-500 hover:bg-teal-400 transition duration-200 text-white rounded-lg"
+          @click="isErrorDialogVisible = false"
+        >
+          OK
+        </button>
+      </div>
+    </template>
+  </SimpleModalDialog>
 </template>
