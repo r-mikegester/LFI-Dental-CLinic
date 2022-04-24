49e0c94d3e46beb16d07a30f05f6712c7819cad5
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-24 14:34:20 +0800

    deleteMessage.js: throw HttpError if response is not OK

--- src/composables/api/deleteMessage.js
+++ src/composables/api/deleteMessage.js
@@ -1,5 +1,9 @@
+import HttpError from "../helpers/HttpError";
+
 export default async (messageUid) => {
   const link = `http://localhost:5000/messages/${messageUid}/delete`;
   const response = await fetch(link);
-  await response.json();
+  const data = await response.json();
+
+  if (!response.ok) throw new HttpError(response.status, data.message);
 };
