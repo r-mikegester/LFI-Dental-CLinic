168e0a50427bff0d4a8f6181b55a52b7b5027474
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-25 00:21:12 +0800

    route: add route validation to admin pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../views/HomePage.vue";
+import redirectToAdminLoginIfNotLoggedInUser from "../composables/route-validation/redirectToAdminLoginIfNotLoggedInUser";
+import redirectToAdminLoginIfNotAdminUser from "../composables/route-validation/redirectToAdminLoginIfNotAdminUser";
+import redirectToAdminHomePageIfAdminUser from "../composables/route-validation/redirectToAdminHomePageIfAdminUser";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,61 +28,102 @@ const router = createRouter({
       path: "/admin",
       name: "Admin Home Page",
       component: () => import("../views/admin/HomePage.vue"),
+      beforeEnter: [
+        redirectToAdminLoginIfNotLoggedInUser,
+        redirectToAdminLoginIfNotAdminUser,
+      ],
     },
     {
       path: "/admin/login",
       name: "Admin Login Page",
       component: () => import("../views/admin/LoginPage.vue"),
+      beforeEnter: [redirectToAdminHomePageIfAdminUser],
     },
     {
       path: "/admin/schedules",
       name: "Admin Schedules Page",
       component: () => import("../views/admin/SchedulesPage.vue"),
+      beforeEnter: [
+        redirectToAdminLoginIfNotLoggedInUser,
+        redirectToAdminLoginIfNotAdminUser,
+      ],
     },
     {
       path: "/admin/appointments",
       name: "Admin Appointments Page",
       component: () => import("../views/admin/AppointmentsPage.vue"),
+      beforeEnter: [
+        redirectToAdminLoginIfNotLoggedInUser,
+        redirectToAdminLoginIfNotAdminUser,
+      ],
     },
     {
       path: "/admin/patient-records",
       name: "Admin Patient Records Page",
       component: () =>
         import("../views/admin/patient-records/PatientRecordsPage.vue"),
+      beforeEnter: [
+        redirectToAdminLoginIfNotLoggedInUser,
+        redirectToAdminLoginIfNotAdminUser,
+      ],
     },
     {
       path: "/admin/patient-records/:uid/medical-chart",
       name: "Admin Medical Chart Page",
       component: () =>
         import("../views/admin/patient-records/MedicalChartPage.vue"),
+      beforeEnter: [
+        redirectToAdminLoginIfNotLoggedInUser,
+        redirectToAdminLoginIfNotAdminUser,
+      ],
     },
     {
       path: "/admin/patient-records/:uid/dental-chart",
       name: "Admin Dental Chart Page",
       component: () =>
         import("../views/admin/patient-records/DentalChartPage.vue"),
+      beforeEnter: [
+        redirectToAdminLoginIfNotLoggedInUser,
+        redirectToAdminLoginIfNotAdminUser,
+      ],
     },
     {
       path: "/admin/patient-records/:uid/treatments",
       name: "Admin Dental Treatments Page",
       component: () =>
         import("../views/admin/patient-records/DentalTreatmentsPage.vue"),
+      beforeEnter: [
+        redirectToAdminLoginIfNotLoggedInUser,
+        redirectToAdminLoginIfNotAdminUser,
+      ],
     },
     {
       path: "/admin/patient-records/:uid/procedures",
       name: "Admin Procedures Page",
       component: () =>
         import("../views/admin/patient-records/ProceduresPage.vue"),
+      beforeEnter: [
+        redirectToAdminLoginIfNotLoggedInUser,
+        redirectToAdminLoginIfNotAdminUser,
+      ],
     },
     {
       path: "/admin/messages",
       name: "Admin Messages Page",
       component: () => import("../views/admin/MessagesPage.vue"),
+      beforeEnter: [
+        redirectToAdminLoginIfNotLoggedInUser,
+        redirectToAdminLoginIfNotAdminUser,
+      ],
     },
     {
       path: "/admin/account-settings",
       name: "Admin Account Settings Page",
       component: () => import("../views/admin/AccountSettingsPage.vue"),
+      beforeEnter: [
+        redirectToAdminLoginIfNotLoggedInUser,
+        redirectToAdminLoginIfNotAdminUser,
+      ],
     },
   ],
 });
