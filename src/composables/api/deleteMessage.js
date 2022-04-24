d2ec3d0ca91fd5fb9518781182080c69074c2254
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-24 14:36:11 +0800

    deleteMessage.js: send HTTP DELETE request, instead of default (GET)

--- src/composables/api/deleteMessage.js
+++ src/composables/api/deleteMessage.js
@@ -2,7 +2,9 @@ import HttpError from "../helpers/HttpError";
 
 export default async (messageUid) => {
   const link = `http://localhost:5000/messages/${messageUid}/delete`;
-  const response = await fetch(link);
+  const response = await fetch(link, {
+    method: "DELETE",
+  });
   const data = await response.json();
 
   if (!response.ok) throw new HttpError(response.status, data.message);
