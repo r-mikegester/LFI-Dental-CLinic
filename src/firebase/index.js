c8ffe98fe7596d8c819d0377cc6688c7477ed028
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-24 15:46:58 +0800

    firebase: add files to initialize firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD79pJh6eYIiLORjlrK3YYVD_Ty-ZcdcOk",
+  authDomain: "lfidentalclinic.firebaseapp.com",
+  projectId: "lfidentalclinic",
+  storageBucket: "lfidentalclinic.appspot.com",
+  messagingSenderId: "890904233316",
+  appId: "1:890904233316:web:91f10ceb437625eb296671",
+  measurementId: "G-G3RBJ4LHMB",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
