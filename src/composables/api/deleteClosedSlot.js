2e092103b759a6e0fa20dab75eaa4f73e82c0fa1
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-28 23:08:10 +0800

    composables/deleteClosedSlot.js: add composable

---
+++ src/composables/api/deleteClosedSlot.js
@@ -0,0 +1,15 @@
+import HttpError from "../helpers/HttpError";
+import ParameterError from "../helpers/ParameterError";
+import backendBaseURL from "./backendBaseURL";
+
+export default async (slotSeconds) => {
+  if (!slotSeconds) throw new ParameterError(slotSeconds);
+
+  const link = `${backendBaseURL}/schedules/closed/${slotSeconds}`;
+  const response = await fetch(link, {
+    method: "DELETE",
+  });
+  const data = await response.json();
+
+  if (!response.ok) throw new HttpError(response.status, data.message);
+};
