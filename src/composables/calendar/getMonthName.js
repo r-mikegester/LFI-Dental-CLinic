24d40a517c97af5df657a0b4fead7822fdf4fc03
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-28 23:01:51 +0800

    composables/getMonthName.js: add composable

---
+++ src/composables/calendar/getMonthName.js
@@ -0,0 +1,18 @@
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export default (monthIndex) => {
+  return monthNames[monthIndex];
+};
