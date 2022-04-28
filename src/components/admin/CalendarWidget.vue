e05ee3dcd5eff09fbea35ab2815b135b3d5bb707
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-28 23:09:10 +0800

    admin/CalendarWidget.vue: add component

---
+++ src/components/admin/CalendarWidget.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="border border-teal-500 rounded-xl max-w-fit">
+    <!-- Days -->
+    <div class="grid grid-cols-[repeat(7,_minmax(0,_7rem))] gap-4 pt-4 px-4">
+      <div
+        class="text-center text-ellipsis overflow-hidden border border-teal-500"
+      >
+        Sunday
+      </div>
+      <div
+        class="text-center text-ellipsis overflow-hidden border border-teal-500"
+      >
+        Monday
+      </div>
+      <div
+        class="text-center text-ellipsis overflow-hidden border border-teal-500"
+      >
+        Tuesday
+      </div>
+      <div
+        class="text-center text-ellipsis overflow-hidden border border-teal-500"
+      >
+        Wednesday
+      </div>
+      <div
+        class="text-center text-ellipsis overflow-hidden border border-teal-500"
+      >
+        Thursday
+      </div>
+      <div
+        class="text-center text-ellipsis overflow-hidden border border-teal-500"
+      >
+        Friday
+      </div>
+      <div
+        class="text-center text-ellipsis overflow-hidden border border-teal-500"
+      >
+        Saturday
+      </div>
+    </div>
+    <!-- Dates -->
+    <div
+      class="grid grid-cols-[repeat(7,_minmax(0,_7rem))] gap-4 p-4 auto-rows-[5rem]"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
