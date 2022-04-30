b0d66a75b5198e6594a12c02fe61c3050b45ecd4
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-30 14:01:01 +0800

    SimpleModalDialog.vue: add shadow to dialog box

--- src/components/admin/SimpleModalDialog.vue
+++ src/components/admin/SimpleModalDialog.vue
@@ -3,7 +3,7 @@
   <div class="fixed inset-0 bg-slate-900/30 flex justify-center items-center">
     <!-- Modal box -->
     <div
-      class="max-w-prose min-w-[30rem] px-6 py-4 bg-white text-sky-600 font-[Poppins] rounded-lg"
+      class="max-w-prose min-w-[30rem] px-6 py-4 bg-white text-sky-600 font-[Poppins] rounded-lg shadow-md"
     >
       <div>
         <slot name="header"></slot>
