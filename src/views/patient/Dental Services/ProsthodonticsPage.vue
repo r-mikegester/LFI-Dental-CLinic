961a3a75feb27781856c590af3d49145e6d7650e
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-01 22:41:09 +0800

    ProsthodonticsPage.vue: add design

---
+++ src/views/patient/Dental Services/ProsthodonticsPage.vue
@@ -0,0 +1,96 @@
+<script setup>
+import BaseLayout from "../../../components/patient/BaseLayout.vue";
+import heroImage from "../../../assets/img/bgs/ContactUs.jpg";
+</script>
+
+<template>
+  <BaseLayout>
+    <!-- Hero section -->
+    <section
+      class="h-[40vh] bg-cover bg-center"
+      :style="{
+        backgroundImage: `url(${heroImage})`,
+      }"
+    ></section>
+    <!-- Page title -->
+    <section class="bg-teal-500 py-3">
+      <h3 class="font-bold text-3xl text-white text-center">Prosthodontics</h3>
+    </section>
+    <!-- Main content -->
+    <section>
+      <div class="max-w-6xl mx-auto px-6 py-24">
+        <article class="grid md:grid-cols-2 max-w-4xl mx-auto mb-24">
+          <div class="flex flex-col justify-center text-justify md:pl-12">
+            <h4 class="text-2xl font-bold mb-3">Partial Denture</h4>
+            <p class="mb-3">
+              This denture is an oral prosthetic device that replace some of
+              missing teeth. This is also removable denture.
+            </p>
+            <div>
+              <button
+                type="button"
+                class="rounded-full px-3 py-1 uppercase border border-sky-600"
+              >
+                Get this Service
+              </button>
+            </div>
+          </div>
+          <div class="mt-6 md:mt-0">
+            <img
+              src="../../../assets/img/dental-services/partial-denture.jpg"
+              class="mx-auto w-full max-w-xs"
+            />
+          </div>
+        </article>
+        <article class="grid md:grid-cols-2 max-w-4xl mx-auto mb-24">
+          <div class="mb-6 md:mb-0">
+            <img
+              src="../../../assets/img/dental-services/jacket-crown.jpg"
+              class="mx-auto w-full max-w-xs"
+            />
+          </div>
+          <div class="flex flex-col justify-center text-justify md:pr-12">
+            <h4 class="text-2xl font-bold mb-3">Jacket Crown</h4>
+            <p class="mb-3">
+              A full porcelain ceramic covered crown which is used to protect
+              the entire surface of a tooth. Crowns are an ideal way to rebuild
+              teeth which have been broken or weakened by decay or large
+              fillings.
+            </p>
+            <div>
+              <button
+                type="button"
+                class="rounded-full px-3 py-1 uppercase border border-sky-600"
+              >
+                Get this Service
+              </button>
+            </div>
+          </div>
+        </article>
+        <article class="grid md:grid-cols-2 max-w-4xl mx-auto">
+          <div class="flex flex-col justify-center text-justify md:pl-12">
+            <h4 class="text-2xl font-bold mb-3">Fixed Bridge</h4>
+            <p class="mb-3">
+              a partial denture that is secured permanently in the mouth by
+              being cemented to the adjacent teeth or roots.
+            </p>
+            <div>
+              <button
+                type="button"
+                class="rounded-full px-3 py-1 uppercase border border-sky-600"
+              >
+                Get this Service
+              </button>
+            </div>
+          </div>
+          <div class="mt-6 md:mt-0">
+            <img
+              src="../../../assets/img/dental-services/fixed-bridge.png"
+              class="mx-auto w-full max-w-xs"
+            />
+          </div>
+        </article>
+      </div>
+    </section>
+  </BaseLayout>
+</template>
