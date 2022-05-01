0f1d2e2f9b554c52a856e2ef27850cdebbb0be08
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-01 22:39:33 +0800

    GeneralDentistryPage.vue: add design

---
+++ src/views/patient/Dental Services/GeneralDentistryPage.vue
@@ -0,0 +1,72 @@
+<script setup>
+import heroImage from "../../../assets/img/bgs/ContactUs.jpg";
+import BaseLayout from "../../../components/patient/BaseLayout.vue";
+</script>
+
+<template>
+  <BaseLayout>
+    <!-- Hero section -->
+    <section
+      class="h-[40vh] bg-cover bg-center"
+      :style="{
+        backgroundImage: `url(${heroImage})`,
+      }"
+    ></section>
+    <!-- Page title -->
+    <section class="bg-teal-500 py-3">
+      <h3 class="font-bold text-3xl text-white text-center">
+        General Dentistry
+      </h3>
+    </section>
+    <section>
+      <div class="max-w-6xl mx-auto px-6 py-24">
+        <article class="grid md:grid-cols-2 max-w-prose mx-auto mb-24">
+          <div class="mb-6 md:mb-0">
+            <img
+              src="../../../assets/img/dental-services/oral-prophylaxis.jpg"
+              class="mx-auto w-full max-w-xs"
+            />
+          </div>
+          <div class="flex flex-col justify-center text-center md:pl-6">
+            <h4 class="text-2xl font-bold mb-3">Oral Prophylaxis</h4>
+            <p class="mb-3">
+              A thorough examination of your oral health combined with a scale
+              and clean and can be conducted by a dentist or a dental hygienist.
+            </p>
+            <div>
+              <button
+                type="button"
+                class="rounded-full px-3 py-1 uppercase border border-sky-600"
+              >
+                Get this Service
+              </button>
+            </div>
+          </div>
+        </article>
+        <article class="grid md:grid-cols-2 max-w-prose mx-auto">
+          <div class="flex flex-col justify-center text-center md:pr-6">
+            <h4 class="text-2xl font-bold mb-3">Tooth Restoration</h4>
+            <p class="mb-3">
+              This can help you get your teeth back to their original function
+              while also preventing further decay.
+            </p>
+            <div class="mb-6 md:mb-0">
+              <button
+                type="button"
+                class="rounded-full px-3 py-1 uppercase border border-sky-600"
+              >
+                Get this Service
+              </button>
+            </div>
+          </div>
+          <div>
+            <img
+              src="../../../assets/img/dental-services/tooth-restoration.jpg"
+              class="mx-auto w-full max-w-xs"
+            />
+          </div>
+        </article>
+      </div>
+    </section>
+  </BaseLayout>
+</template>
