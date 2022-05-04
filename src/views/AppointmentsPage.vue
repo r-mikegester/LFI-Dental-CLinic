a0228933c205ec65ec78d42cdc17bc581ef7f72d
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-05 01:36:10 +0800

    AppointmentsPage.vue: add design using children routes

--- src/views/AppointmentsPage.vue
+++ src/views/AppointmentsPage.vue
@@ -1,27 +1,12 @@
 <script setup>
+import BaseLayout from "../components/patient/BaseLayout.vue";
+import HeroSection from "../components/patient/HeroSection.vue";
 import { RouterView } from "vue-router";
-import NavBar from "../components/patients-view/NavBar.vue";
-import FooterSection from "../components/patients-view/FooterSection.vue";
 </script>
-<template>
-  <header>
-    <NavBar />
-  </header>
 
-  <div>
-    <div id="hero" class="bg-fixed">
-      <h5
-        class="relative z-30 top-52 justify-center text-5xl font-bold text-center text-white"
-      >
-        SET AN APPOINTMENT
-      </h5>
-      <img
-        src="../assets/img/bgs/ContactUs.jpg"
-        class="w-full bg-fixed object-fit-cover object-center filter brightness-50"
-        style="height: 50vh; background-position: center background-size: 100% 100%;"
-      />
-    </div>
-  </div>
-  <RouterView />
-  <FooterSection />
+<template>
+  <BaseLayout>
+    <HeroSection>Book an Appointment</HeroSection>
+    <RouterView />
+  </BaseLayout>
 </template>
