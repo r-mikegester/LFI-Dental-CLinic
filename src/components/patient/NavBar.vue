f048400b468276aa529b1729906f7783432d5ba6
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-05 01:35:05 +0800

    patient/NavBar.vue: remove z-index

--- src/components/patient/NavBar.vue
+++ src/components/patient/NavBar.vue
@@ -8,7 +8,7 @@ const dentalServicesMenuIsVisible = ref(false);
 </script>
 
 <template>
-  <nav class="sticky top-0 bg-white shadow z-10">
+  <nav class="sticky top-0 bg-white shadow">
     <div class="max-w-7xl mx-auto px-6 py-3 lg:flex lg:justify-between">
       <!-- Website logo -->
       <div class="flex justify-between">
