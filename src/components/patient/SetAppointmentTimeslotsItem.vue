811edc85cd84d40d0d32bfe133d203c1ec3c85fd
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-05 01:21:07 +0800

    SetAppointmentTimeslotItem.vue: add component

---
+++ src/components/patient/SetAppointmentTimeslotsItem.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+  hours: Number,
+  minutes: Number,
+  isSelected: Boolean,
+  isClosed: Boolean,
+  isTaken: Boolean,
+});
+</script>
+
+<template>
+  <div
+    class="border border-teal-500 text-center py-1"
+    :class="{
+      'bg-gray-300': isClosed,
+      'border-gray-600': isClosed,
+      'bg-sky-600': isTaken,
+      'text-white': isTaken,
+      'bg-teal-500': isSelected,
+      'text-white': isSelected,
+    }"
+  >
+    <span v-if="hours > 12">{{ hours - 12 }}</span>
+    <span v-else>{{ hours }}</span
+    >:<span v-if="minutes < 10">0{{ minutes }}</span>
+    <span v-else>{{ minutes }}</span>
+
+    <span v-if="hours > 12"> pm</span>
+    <span v-else> am</span>
+  </div>
+</template>
