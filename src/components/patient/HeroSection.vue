33454b58987abe24d94954f4b203640f7aa9b515
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-05 01:34:27 +0800

    HeroSection.vue: use absolute-positioned img element, instead of CSS background

--- src/components/patient/HeroSection.vue
+++ src/components/patient/HeroSection.vue
@@ -1,16 +1,14 @@
 <template>
-  <section
-    class="patient-view-hero-section h-[40vh] my-1 relative before:block before:h-full before:bg-no-repeat before:bg-center before:bg-cover before:blur-sm"
-  >
+  <section class="patient-view-hero-section h-[40vh] mt-1 relative -z-[1]">
+    <img
+      src="../../assets/img/bgs/ContactUs.jpg"
+      alt="Woman in a dentist chair smiling in front of a dentist"
+      class="absolute top-0 left-0 w-full h-full object-cover blur-sm"
+    />
     <div
-      class="absolute top-0 left-0 h-full w-full flex items-center justify-center uppercase text-4xl text-white font-bold [text-shadow:_rgba(0,_0,_0,_0.2)_0_2px_2px]"
+      class="absolute top-0 left-0 h-full w-full flex items-center justify-center uppercase text-4xl text-white font-bold"
     >
       <slot></slot>
     </div>
   </section>
 </template>
-<style>
-.patient-view-hero-section::before {
-  background-image: url("../../assets/img/bgs/ContactUs.jpg");
-}
-</style>
