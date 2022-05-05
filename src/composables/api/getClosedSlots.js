206dd9b22f3272e9dfe73414b72b7d4770d4649e
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-05 19:28:11 +0800

    getClosedSlots.js: use year-month format

--- src/composables/api/getClosedSlots.js
+++ src/composables/api/getClosedSlots.js
@@ -1,7 +1,7 @@
 import backendBaseURL from "./backendBaseURL";
 
-export default async (startSeconds, endSeconds) => {
-  const link = `${backendBaseURL}/timeslots/closed/${startSeconds}/${endSeconds}`;
+export default async (year, month) => {
+  const link = `${backendBaseURL}/timeslots/${year}/${month}/closed`;
   const response = await fetch(link);
   const data = await response.json();
   return data.payload;
