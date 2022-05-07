c552bc80bb5d24d29a73b595fc86ce44c7460bf2
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-08 02:14:34 +0800

    getUnavailableSlots.js: use more accurate parameter names

--- src/composables/api/getUnavailableSlots.js
+++ src/composables/api/getUnavailableSlots.js
@@ -1,7 +1,7 @@
 import backendBaseURL from "./backendBaseURL";
 
-export default async (startSeconds, endSeconds) => {
-  const link = `${backendBaseURL}/timeslots/unavailable/${startSeconds}/${endSeconds}`;
+export default async (year, month) => {
+  const link = `${backendBaseURL}/timeslots/unavailable/${year}/${month}`;
   const response = await fetch(link);
   const data = await response.json();
   return data.payload;
