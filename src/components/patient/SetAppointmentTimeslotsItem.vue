2d82033ca5e335ce4a01f8104f1eb1ee3378e0da
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-08 02:13:53 +0800

    SetAppointmentTimeslotItem.vue: make sure text is centered

--- src/components/patient/SetAppointmentTimeslotsItem.vue
+++ src/components/patient/SetAppointmentTimeslotsItem.vue
@@ -10,7 +10,7 @@ defineProps({
 
 <template>
   <div
-    class="border border-teal-500 text-center py-1"
+    class="border border-teal-500 text-center py-1 flex justify-center items-center"
     :class="{
       'bg-gray-300': isClosed,
       'border-gray-600': isClosed,
@@ -19,12 +19,15 @@ defineProps({
       'text-white': isTaken || isSelected,
     }"
   >
-    <span v-if="hours > 12">{{ hours - 12 }}</span>
-    <span v-else>{{ hours }}</span
-    >:<span v-if="minutes < 10">0{{ minutes }}</span>
-    <span v-else>{{ minutes }}</span>
+    <div>
+      <span v-if="hours > 12">{{ hours - 12 }}</span>
+      <span v-else>{{ hours }}</span>
+      <span>:</span>
+      <span v-if="minutes < 10">0{{ minutes }}</span>
+      <span v-else>{{ minutes }}</span>
 
-    <span v-if="hours > 12"> pm</span>
-    <span v-else> am</span>
+      <span v-if="hours > 12"> pm</span>
+      <span v-else> am</span>
+    </div>
   </div>
 </template>
