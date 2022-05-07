7a20f925046bc4aa41e317dc733299ef820aa8a7
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-07 21:25:05 +0800

    ParameterError.js: set parameter name as first parameter, instead of parameter itself
    Apparently, it is not possible to get the variable name of a
    passed argument. So, we just ask for the actual parameter name
    in String format when constructing the object.

--- src/composables/helpers/ParameterError.js
+++ src/composables/helpers/ParameterError.js
@@ -1,6 +1,5 @@
 export default class ParameterError extends Error {
-  constructor(parameter, errorMessage = "") {
-    const parameterName = Object.keys({ parameter })[0];
+  constructor(parameterName, errorMessage = "") {
     const message = errorMessage
       ? `Invalid parameter ${parameterName}: ${errorMessage}`
       : `Invalid or missing parameter ${parameterName}`;
