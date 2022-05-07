723235ce30235a98803d3426b06b8fc3a4d35954
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-07 21:32:34 +0800

    FillInMedicalChart.vue: add child component for filling up medical chart on account creation

---
+++ src/components/booking/FIllInMedicalChart.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useRouter } from "vue-router";
+import { useAppointmentDetailsStore } from "../../stores/appointmentDetails";
+import newAppointment from "../../composables/api/newAppointment";
+import MedicalChart from "../shared/MedicalChart.vue";
+
+const appointmentDetailsStore = useAppointmentDetailsStore();
+const router = useRouter();
+const redirectToAppointmentHistoryPage = async () => {
+  await newAppointment(
+    appointmentDetailsStore.getSlotSeconds,
+    appointmentDetailsStore.getService
+  );
+  router.push({
+    name: "Patient Appointment History Page",
+  });
+};
+</script>
+
+<template>
+  <div class="max-w-4xl mx-auto px-3 py-12">
+    <MedicalChart @formSubmitted="redirectToAppointmentHistoryPage()" />
+  </div>
+</template>
