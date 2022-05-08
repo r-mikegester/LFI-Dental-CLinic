6a628298673c02ab2beb7223311c774a98a5234d
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-09 00:16:45 +0800

    getHoursMinutesOfTimeslot.js: add composable

---
+++ src/composables/calendar/getHoursMinutesOfTimeslot.js
@@ -0,0 +1,12 @@
+export default (timeslot) => {
+  const fullTime = new Date(timeslot * 1000)
+    .toLocaleString("en-us", {
+      timeZone: "Asia/Manila",
+    })
+    .split(",")[1];
+
+  const hours = fullTime.split(":")[0];
+  const minutes = fullTime.split(":")[1];
+  const ampm = fullTime.split(" ")[2];
+  return `${hours}:${minutes} ${ampm}`;
+};
