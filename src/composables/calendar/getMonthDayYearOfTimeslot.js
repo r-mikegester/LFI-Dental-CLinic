ee9290b98a84d478b2b99a1799734bb941abcf98
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-09 00:16:09 +0800

    getMonthDayYearOfTimeslot.js: add composable

---
+++ src/composables/calendar/getMonthDayYearOfTimeslot.js
@@ -0,0 +1,8 @@
+export default (timeslot) => {
+  return new Date(timeslot * 1000).toLocaleString("en-us", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    timeZone: "Asia/Manila",
+  });
+};
