cb6f03dd085ad9fb335f73be50970d63c218efff
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-09 01:39:55 +0800

    getDateToday.js: add composable

---
+++ src/composables/calendar/getDateToday.js
@@ -0,0 +1,27 @@
+import { DateTime } from "luxon";
+
+export default () => {
+  const date = new Date();
+  const isoDateStr = DateTime.fromObject({
+    year: parseInt(
+      date.toLocaleString("en-us", {
+        timeZone: "Asia/Manila",
+        year: "numeric",
+      })
+    ),
+    month: parseInt(
+      date.toLocaleString("en-us", {
+        timeZone: "Asia/Manila",
+        month: "numeric",
+      })
+    ),
+    day: parseInt(
+      date.toLocaleString("en-us", {
+        timeZone: "Asia/Manila",
+        day: "numeric",
+      })
+    ),
+  }).toISO();
+
+  return new Date(isoDateStr);
+};
