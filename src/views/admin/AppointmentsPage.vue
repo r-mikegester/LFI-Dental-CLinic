86899ab7570a53a2c79ca04e42bad762289d0993
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-09 00:19:37 +0800

    admin/AppointmentsPage.vue: add design and functionality

--- src/views/admin/AppointmentsPage.vue
+++ src/views/admin/AppointmentsPage.vue
@@ -1,9 +1,191 @@
 <script setup>
+import { nextTick, onMounted, reactive, ref } from "vue";
 import BaseLayout from "../../components/admin/BaseLayout.vue";
+import CalendarWidget from "../../components/admin/CalendarWidget.vue";
+import AppointmentsPageCalendarItem from "../../components/admin/AppointmentsPageCalendarItem.vue";
+import AppointmentsPageAppointmentItem from "../../components/admin/AppointmentsPageAppointmentItem.vue";
+import { useScheduleCalendarStore } from "../../stores/scheduleCalendar";
+import getAllAppointments from "../../composables/api/getAllAppointments";
+import getMonthIndex from "../../composables/calendar/getMonthIndex";
+
+const selected = reactive({
+  month: "",
+  year: "",
+  date: "",
+});
+const appointmentsList = ref([]);
+
+const scheduleCalendar = useScheduleCalendarStore();
+const isFinishLoading = ref(false);
+
+onMounted(async () => {
+  const currDate = new Date();
+
+  // Make sure we are getting the current month
+  // from UTC+8 (Asia/Manila).
+  selected.month = currDate.toLocaleString("en-US", {
+    timeZone: "Asia/Manila",
+    month: "long",
+  });
+
+  // Make sure we are getting the current year
+  // from UTC+8 (Asia/Manila).
+  selected.year = currDate.toLocaleString("en-US", {
+    timeZone: "Asia/Manila",
+    year: "numeric",
+  });
+
+  // Set new month and year in the store,
+  // then build the list of calendar items
+  // with everything unselected.
+  await scheduleCalendar.setMonthAndYear(selected.month, selected.year);
+  for (const n of Array(scheduleCalendar.getDayCount).keys()) {
+    calendarItems.value.push({
+      date: n + 1,
+      selected: false,
+    });
+  }
+
+  // Populate appointments list
+  appointmentsList.value = await getAllAppointments(
+    selected.year,
+    getMonthIndex(selected.month) + 1
+  );
+
+  // Make sure setMonthAndYear is finished before we show the page.
+  await nextTick();
+  isFinishLoading.value = true;
+});
+
+const onChangeMonthOrYear = async () => {
+  // Hide the calendar temporarily.
+  isFinishLoading.value = false;
+
+  // Set new month in the store.
+  await scheduleCalendar.setMonthAndYear(selected.month, selected.year);
+
+  // Reset calendar items, then build
+  // a new list of calendar items with all
+  // items unselected.
+  calendarItems.value = [];
+  for (const n of Array(scheduleCalendar.getDayCount).keys()) {
+    calendarItems.value.push({
+      date: n + 1,
+      selected: false,
+    });
+  }
+
+  // Populate appointments list
+  appointmentsList.value = [];
+  appointmentsList.value = await getAllAppointments(
+    selected.year,
+    getMonthIndex(selected.month) + 1
+  );
+
+  // Wait for all request to flush,
+  // then show the calendar again.
+  await nextTick();
+  isFinishLoading.value = true;
+};
+
+// Create list of calendar items, with click state managed by parent.
+const calendarItems = ref([]);
+
+const onCalendarItemSelected = (selectedCalendarItem) => {
+  // Iterate through the list of calendar items, and mark
+  // the selected item as selected = true. Everything else
+  // is marked selected = false.
+  selected.date = selectedCalendarItem.date;
+  calendarItems.value = calendarItems.value.map((calendarItem) => ({
+    ...calendarItem,
+    selected: calendarItem === selectedCalendarItem ? true : false,
+  }));
+};
 </script>
 
 <template>
   <BaseLayout>
-    <h1 class="text-4xl text-center font-bold">Admin Appointments page</h1>
+    <div class="lg:px-6">
+      <h1 class="text-2xl font-semibold mb-3">Appointments</h1>
+      <div v-if="isFinishLoading">
+        <div class="grid gap-4 lg:grid-cols-[8rem_auto] mx-auto">
+          <div>
+            <div class="border border-teal-500 px-2 py-1 rounded-full mb-3">
+              <select
+                class="w-full bg-transparent"
+                v-model="selected.month"
+                @change="onChangeMonthOrYear()"
+              >
+                <option>January</option>
+                <option>February</option>
+                <option>March</option>
+                <option>April</option>
+                <option>May</option>
+                <option>June</option>
+                <option>July</option>
+                <option>August</option>
+                <option>September</option>
+                <option>October</option>
+                <option>November</option>
+                <option>December</option>
+              </select>
+            </div>
+            <div class="border border-teal-500 px-2 py-1 rounded-full">
+              <select
+                class="w-full bg-transparent"
+                v-model="selected.year"
+                @change="onChangeMonthOrYear()"
+              >
+                <option>2022</option>
+                <option>2023</option>
+                <option>2024</option>
+                <option>2025</option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <!-- Calendar -->
+            <CalendarWidget>
+              <div
+                class="border border-teal-500 text-right px-2"
+                v-for="_ in scheduleCalendar.getOffset"
+                :key="_"
+              ></div>
+              <AppointmentsPageCalendarItem
+                class="border border-teal-500 text-right px-2"
+                v-for="calendarItem in calendarItems"
+                :key="calendarItem.date"
+                :year="selected.year"
+                :month="selected.month"
+                :date="calendarItem.date"
+                :isSelected="calendarItem.selected"
+                @click="onCalendarItemSelected(calendarItem)"
+              />
+            </CalendarWidget>
+          </div>
+        </div>
+        <div v-if="appointmentsList.length > 0">
+          <div
+            class="grid grid-cols-4 gap-4 py-4 px-6 bg-teal-500/40 mt-8 font-semibold text-sky-700"
+          >
+            <div>Date</div>
+            <div>Full name</div>
+            <div>Service</div>
+            <div>Time</div>
+          </div>
+          <AppointmentsPageAppointmentItem
+            v-for="appointment in appointmentsList"
+            :key="appointment.timeslot"
+            :timeslot="appointment.timeslot"
+            :service="appointment.service"
+            :patientUid="appointment.patientUid"
+          />
+        </div>
+      </div>
+      <div class="text-2xl font-bold text-center" v-else>
+        Loading calendar...
+      </div>
+      <!-- Queued Appointments Widget -->
+    </div>
   </BaseLayout>
 </template>
