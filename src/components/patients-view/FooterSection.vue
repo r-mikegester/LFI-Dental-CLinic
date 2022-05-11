366be735800d1a78175d3e7d469b074b9a83eac8
Mike Gester <mikegester.sabuga023@gmail.com>
2022-05-11 18:55:13 +0800

    FooterSection.vue Add some tweaks Partially Complete

--- src/components/patients-view/FooterSection.vue
+++ src/components/patients-view/FooterSection.vue
@@ -2,7 +2,7 @@
   <!-- Footer -->
   <footer
     id="footer"
-    class="h-full p-4 bg-white mt-50 pb-100 sm:p-6 dark:bg-gray-800"
+    class="h-full p-4 bg-white mt-50 pb-100 sm:p-6"
     style="background-color: rgba(20, 184, 166, 0.4)"
   >
     <div class="md:flex md:justify-between sm:justify-items-center">
@@ -14,87 +14,44 @@
             height="150px"
             class="px-1 ml-10 -mb-10"
           />
-          <div
-            class="px-5 ml-20 text-2xl font-bold text-sky-700 dark:text-white"
-          >
-            Dental Clinic
+          <div class="px-5 ml-20 text-2xl font-bold text-sky-700">
+            LFI Dental Clinic
           </div>
         </a>
       </div>
+
       <div
         class="grid grid-cols-2 gap-8 ml-20 mr-20 sm:gap-20 sm:grid-cols-3 sm:justify-items-center"
       >
         <div>
-          <h2
-            class="mb-4 text-lg font-bold uppercase text-sky-700 dark:text-white"
-          >
+          <h2 class="mb-4 text-lg font-bold uppercase text-sky-700">
             Services
           </h2>
           <ul class="text-gray-600 dark:text-gray-400">
-            <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
-                >Oral Prophylaxis</a
-              >
-            </li>
-            <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
-                >Tooth Restoration</a
-              >
-            </li>
-            <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
-                >Whitening</a
-              >
-            </li>
-            <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
-                >Flouride Treatment</a
-              >
-            </li>
-            <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
-                >Orthodontic Braces</a
-              >
-            </li>
-            <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
-                >Tooth Extraction</a
-              >
-            </li>
-            <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
-                >Complete Denture</a
-              >
-            </li>
-            <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
-                >Partial Denture</a
-              >
-            </li>
-            <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
-                >Jacket Crown</a
-              >
-            </li>
-            <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
-                >Fixed Bridge</a
-              >
-            </li>
+            <li class="font-semibold text-sky-700">Oral Prophylaxis</li>
+            <li class="font-semibold text-sky-700">Tooth Restoration</li>
+            <li class="font-semibold text-sky-700">Whitening</li>
+            <li class="font-semibold text-sky-700">Flouride Treatment</li>
+            <li class="font-semibold text-sky-700">Orthodontic Braces</li>
+            <li class="font-semibold text-sky-700">Tooth Extraction</li>
+            <li class="font-semibold text-sky-700">Complete Denture</li>
+            <li class="font-semibold text-sky-700">Partial Denture</li>
+            <li class="font-semibold text-sky-700">Jacket Crown</li>
+            <li class="font-semibold text-sky-700">Fixed Bridge</li>
           </ul>
         </div>
+
         <div>
-          <h2
-            class="mb-4 text-lg font-bold uppercase text-sky-700 dark:text-white"
-          >
+          <h2 class="mb-4 text-lg font-bold uppercase text-sky-700">
             Quick Links
           </h2>
-          <ul class="text-gray-600 dark:text-gray-400">
+          <ul class="text-gray-600">
             <li>
               <a href="#" class="font-semibold hover:underline text-sky-700"
                 >Home</a
               >
             </li>
+
             <li>
               <a href="#" class="font-semibold hover:underline text-sky-700"
                 >Dental Services</a
@@ -123,26 +80,8 @@
           </ul>
         </div>
         <div>
-          <h2
-            class="mb-4 text-lg font-bold uppercase text-sky-700 dark:text-white"
-          >
-            Legal
-          </h2>
+          <h2 class="mb-4 text-lg font-bold uppercase text-sky-700">Extras</h2>
           <ul class="text-gray-600 dark:text-gray-400">
-            <li>
-              <a
-                href="Privacypolicy.html"
-                class="font-semibold hover:underline text-sky-700"
-                >Privacy Policy</a
-              >
-            </li>
-            <li>
-              <a
-                href="TnC.html"
-                class="font-semibold hover:underline text-sky-700"
-                >Terms &amp; Conditions</a
-              >
-            </li>
             <li>
               <a
                 href="Faqs.html"
@@ -154,8 +93,9 @@
         </div>
       </div>
     </div>
-    <hr class="my-4 border-sky-700 sm:mx-auto dark:border-gray-700 lg:mt-10" />
-    <div class="p-2 sm:flex sm:items-center sm:justify-between">
+    <hr class="my-4 border-sky-700 sm:mx-auto dark:border-sky-700 lg:mt-10" />
+
+    <div class="p-2 sm:flex sm:items-center sm:justify-center">
       <span
         class="text-sm font-semibold text-gray-500 sm:text-center dark:text-gray-400"
         >© 2022
