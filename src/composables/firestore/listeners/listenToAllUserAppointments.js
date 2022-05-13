021bcbc2296876a8ee5571973da214e89cca4f33
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 10:25:34 +0800

    listenToAllUserAppointments.js: add composable

---
+++ src/composables/firestore/listeners/listenToAllUserAppointments.js
@@ -0,0 +1,34 @@
+import {
+  collectionGroup,
+  getFirestore,
+  onSnapshot,
+  query,
+  where,
+} from "@firebase/firestore";
+import { onUnmounted, ref } from "vue";
+
+const db = getFirestore();
+export default () => {
+  const userAppointments = ref(null);
+
+  const q = query(
+    collectionGroup(db, "appointments"),
+    where("procedureVisible", "==", "requesting")
+  );
+  const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    userAppointments.value = [];
+    querySnapshot.forEach((doc) => {
+      userAppointments.value.push({
+        uid: doc.id,
+        ...doc.data(),
+      });
+      console.log(userAppointments.value);
+    });
+  });
+
+  onUnmounted(() => {
+    unsubscribe();
+  });
+
+  return userAppointments;
+};
