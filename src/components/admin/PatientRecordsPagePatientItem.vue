7dfbb8ba39fca0d8d364e902320c54ad7fcb1851
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 11:40:33 +0800

    PatientRecordsPagePatientItem.vue: add component

---
+++ src/components/admin/PatientRecordsPagePatientItem.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  uid: String,
+  email: String,
+  fullName: String,
+});
+</script>
+
+<template>
+  <div
+    class="border-b border-teal-500 p-4 gap-4 grid grid-cols-[minmax(0,_1fr)_minmax(0,_2fr)_repeat(3,_minmax(0,_1fr))]"
+  >
+    <div class="overflow-hidden text-ellipsis">
+      {{ fullName }}
+    </div>
+    <div class="overflow-hidden text-ellipsis">
+      {{ email }}
+    </div>
+    <div>
+      <RouterLink :to="{ name: 'Admin Medical Chart Page', params: { uid } }">
+        View
+      </RouterLink>
+    </div>
+    <div>
+      <RouterLink :to="{ name: 'Admin Dental Chart Page', params: { uid } }">
+        View
+      </RouterLink>
+    </div>
+    <div>
+      <RouterLink
+        :to="{ name: 'Admin Dental Treatments Page', params: { uid } }"
+        >View
+      </RouterLink>
+    </div>
+  </div>
+</template>
