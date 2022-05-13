12faa67b66c9d9a8dddd80eb8b58204a0a61c638
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-14 06:15:09 +0800

    profilePicture.js: add store

---
+++ src/stores/profilePicture.js
@@ -0,0 +1,31 @@
+import "../firebase";
+import { getAuth } from "@firebase/auth";
+import { defineStore } from "pinia";
+import getDownloadURL from "../composables/account/getDownloadURL";
+const auth = getAuth();
+
+export const useProfilePictureStore = defineStore({
+  id: "profilePicture",
+  state: () => ({
+    profilePictureDownloadURL: null,
+    profilePictureURL: null,
+    initialized: false,
+  }),
+  getters: {
+    getDownloadURL: (state) => state.profilePictureDownloadURL,
+    getURL: (state) => state.profilePictureURL,
+    isInitialized: (state) => state.initialized,
+  },
+  actions: {
+    async initialize() {
+      const profilePictureURL = auth.currentUser.photoURL;
+      if (profilePictureURL)
+        this.profilePictureDownloadURL = await getDownloadURL(
+          profilePictureURL
+        );
+      else this.profilePictureDownloadURL = "";
+      this.profilePictureURL = profilePictureURL;
+      this.initialized = true;
+    },
+  },
+});
