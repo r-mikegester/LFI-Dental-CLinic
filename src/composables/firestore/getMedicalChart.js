53372bca45e4a13524465e4c40348093e2c70b19
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 10:13:16 +0800

    firestore/getMedicalChart.js: add composable

---
+++ src/composables/firestore/getMedicalChart.js
@@ -0,0 +1,16 @@
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import DatabaseError from "../helpers/DatabaseError";
+
+const db = getFirestore();
+
+export default async (patientUid) => {
+  const docRef = doc(db, `users/${patientUid}/patientRecords/medicalChart`);
+
+  const docSnap = await getDoc(docRef);
+  if (!docSnap.exists)
+    throw new DatabaseError("getting medical chart", "No such document");
+
+  return {
+    ...docSnap.data(),
+  };
+};
