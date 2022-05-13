b5397b7506aadb22bbeb5a414a764d2338fc1ea3
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 11:08:22 +0800

    patient/LoginPage.vue: add view

---
+++ src/views/patient/LoginPage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import BaseLayout from "../../components/patient/BaseLayout.vue";
+import LoginAccount from "../../components/booking/LoginAccount.vue";
+import HeroSection from "../../components/patient/HeroSection.vue";
+</script>
+
+<template>
+  <BaseLayout>
+    <HeroSection>Login to your Account</HeroSection>
+    <LoginAccount />
+  </BaseLayout>
+</template>
