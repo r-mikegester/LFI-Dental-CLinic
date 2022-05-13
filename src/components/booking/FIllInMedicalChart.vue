b73fbc8295993ce5902669537272e9180a635e70
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 11:00:20 +0800

    FillInMedicalChart.js: add function to create new appointment

--- src/components/booking/FIllInMedicalChart.vue
+++ src/components/booking/FIllInMedicalChart.vue
@@ -3,22 +3,66 @@ import { useRouter } from "vue-router";
 import { useAppointmentDetailsStore } from "../../stores/appointmentDetails";
 import newAppointment from "../../composables/api/newAppointment";
 import MedicalChart from "../shared/MedicalChart.vue";
+import { ref } from "vue";
+import updateMedicalChart from "../../composables/api/updateMedicalChart";
+import { getAuth } from "firebase/auth";
 
 const appointmentDetailsStore = useAppointmentDetailsStore();
 const router = useRouter();
-const redirectToAppointmentHistoryPage = async () => {
-  await newAppointment(
-    appointmentDetailsStore.getSlotSeconds,
-    appointmentDetailsStore.getService
-  );
-  router.push({
-    name: "Patient Appointment History Page",
-  });
+
+const isRequiredFieldsValid = (personalInformation) => {
+  if (personalInformation.fullName === "") return false;
+  if (personalInformation.gender === "") return false;
+  if (personalInformation.birthDate === "") return false;
+  if (personalInformation.maritalStatus === "") return false;
+  return true;
+};
+
+const isSubmitDisabled = ref(false);
+
+const auth = getAuth();
+const onSubmit = async (personalInformation, medicalHistory, dentalHistory) => {
+  if (isRequiredFieldsValid(personalInformation)) {
+    isSubmitDisabled.value = true;
+    const patientUid = auth.currentUser.uid;
+    await updateMedicalChart(
+      patientUid,
+      personalInformation,
+      medicalHistory,
+      dentalHistory
+    );
+
+    await newAppointment(
+      appointmentDetailsStore.getSlotSeconds,
+      appointmentDetailsStore.getService
+    );
+
+    router.push({
+      name: "Patient Appointment History Page",
+    });
+  }
 };
 </script>
 
 <template>
-  <div class="max-w-4xl mx-auto px-3 py-12">
-    <MedicalChart @formSubmitted="redirectToAppointmentHistoryPage()" />
+  <div class="max-w-5xl mx-auto px-3 py-12">
+    <MedicalChart>
+      <template
+        #default="{ personalInformation, medicalHistory, dentalHistory }"
+      >
+        <button
+          type="button"
+          class="px-6 py-2 rounded-3xl bg-teal-500 hover:bg-teal-400 transition duration-200 text-white"
+          :class="{
+            'pointer-events-none': isSubmitDisabled,
+            'bg-emerald-200': isSubmitDisabled,
+          }"
+          @click="onSubmit(personalInformation, medicalHistory, dentalHistory)"
+        >
+          <span v-if="isSubmitDisabled">Saving ...</span>
+          <span v-else>Submit</span>
+        </button>
+      </template>
+    </MedicalChart>
   </div>
 </template>
