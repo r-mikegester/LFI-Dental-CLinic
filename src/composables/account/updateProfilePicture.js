3ea81f7e664f98194c8a08d5f0ed98df4dbedd8f
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-14 06:14:49 +0800

    updateProfilePicture.js: add composable

---
+++ src/composables/account/updateProfilePicture.js
@@ -0,0 +1,9 @@
+import { getAuth, updateProfile } from "@firebase/auth";
+
+const auth = getAuth();
+export default async (imagePath) => {
+  await updateProfile(auth.currentUser, {
+    photoURL: imagePath,
+  });
+  await auth.currentUser.reload();
+};
