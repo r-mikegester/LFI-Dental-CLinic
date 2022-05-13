6ac974bc3df377c15aec95f0d4cea6e05623b916
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 10:56:33 +0800

    NavBar.vue: add LFI to website name

--- src/components/patient/NavBar.vue
+++ src/components/patient/NavBar.vue
@@ -19,7 +19,7 @@ const dentalServicesMenuIsVisible = ref(false);
             class="h-12"
           />
           <span class="pl-4 font-bold tracking-widest uppercase text-xl">
-            Dental Clinic
+            LFI Dental Clinic
           </span>
         </RouterLink>
         <div class="lg:hidden flex items-center">
@@ -105,6 +105,22 @@ const dentalServicesMenuIsVisible = ref(false);
           >
             Contact Us
           </RouterLink>
+        </li>
+        <li>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-6 h-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
         </li>
       </ul>
 
