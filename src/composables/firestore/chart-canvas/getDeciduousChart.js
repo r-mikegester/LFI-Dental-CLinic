b0e9ba3cc4e4c26d7bd36a1f5dc6ad3931f45a45
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 10:27:01 +0800

    getDeciduousChart.js: add composable

---
+++ src/composables/firestore/chart-canvas/getDeciduousChart.js
@@ -0,0 +1,14 @@
+import { doc, getDoc, getFirestore } from "@firebase/firestore";
+
+const db = getFirestore();
+
+export default async (patientUid) => {
+  const docRef = doc(db, `users/${patientUid}/patientRecords/deciduousChart`);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists) {
+    const data = docSnap.data();
+    if (data) return data.dataUrl;
+    else return "";
+  } else return "";
+};
