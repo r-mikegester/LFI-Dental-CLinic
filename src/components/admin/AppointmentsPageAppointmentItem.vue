7c9bbc241d1c7bc65f2f4c07622e8b520b763e40
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 11:10:44 +0800

    AppointmentsPageAppointmentItem.vue: add component

--- src/components/admin/AppointmentsPageAppointmentItem.vue
+++ src/components/admin/AppointmentsPageAppointmentItem.vue
@@ -1,23 +1,40 @@
 <script setup>
+import { onMounted, ref } from "vue";
 import getMonthDayYearOfTimeslot from "../../composables/calendar/getMonthDayYearOfTimeslot";
 import getUserProfile from "../../composables/api/getUserProfile";
 import getHoursMinutesOfTimeslot from "../../composables/calendar/getHoursMinutesOfTimeslot";
-import { onMounted, ref } from "vue";
+import setUserAppointmentAttended from "../../composables/api/user-appointment-attended/setUserAppointmentAttended";
+import unsetUserAppointmentAttended from "../../composables/api/user-appointment-attended/unsetUserAppointmentAttended";
 
 const props = defineProps({
   timeslot: String,
   service: String,
   patientUid: String,
+  attended: Boolean,
 });
 
 const userProfile = ref(null);
+const attended = ref(null);
 onMounted(async () => {
   userProfile.value = await getUserProfile(props.patientUid);
+  attended.value = props.attended;
 });
+
+const emit = defineEmits(["attended-changed"]);
+
+const onChange = async () => {
+  if (attended.value) {
+    await setUserAppointmentAttended(props.patientUid, props.timeslot);
+    emit("attended-changed", attended.value, props.timeslot);
+  } else {
+    await unsetUserAppointmentAttended(props.patientUid, props.timeslot);
+    emit("attended-changed", attended.value, props.timeslot);
+  }
+};
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-4 py-4 px-6 border-b border-teal-500">
+  <div class="grid grid-cols-5 gap-4 py-4 px-6 border-b border-teal-500">
     <div class="overflow-hidden text-ellipsis">
       {{ getMonthDayYearOfTimeslot(parseInt(props.timeslot)) }}
     </div>
@@ -31,6 +48,9 @@ onMounted(async () => {
     </div>
     <div class="overflow-hidden text-ellipsis">
       {{ getHoursMinutesOfTimeslot(parseInt(props.timeslot)) }}
+    </div>
+    <div>
+      <input type="checkbox" @change="onChange()" v-model="attended" />
     </div>
   </div>
 </template>
