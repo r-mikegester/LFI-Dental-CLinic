2b4235e2e638867d8e222b46103c4257cdb04e38
Mike Gester <mikegester.sabuga023@gmail.com>
2022-05-13 10:47:02 +0800

    FooterSection.vue All goods Complete Responsive Footer Design

--- src/components/patients-view/FooterSection.vue
+++ src/components/patients-view/FooterSection.vue
@@ -6,28 +6,32 @@
     style="background-color: rgba(20, 184, 166, 0.4)"
   >
     <div class="md:flex md:justify-between sm:justify-items-center">
-      <div class="md:mb-0">
+      <div class="md:mb-0 lg:ml-36">
         <a href="#" class="items-center sm:justify-items-center">
           <img
             src="../../assets/img/LFI-trans.png"
             width="250px"
             height="150px"
-            class="px-1 ml-10 -mb-10"
+            class="px-1 mx-auto -mb-10"
           />
-          <div class="px-5 ml-20 text-2xl font-bold text-sky-700">
+          <div
+            class="px-5 text-2xl font-bold text-center mb-10 md:mb-0 text-sky-700"
+          >
             LFI Dental Clinic
           </div>
         </a>
       </div>
 
       <div
-        class="grid grid-cols-2 gap-8 ml-20 mr-20 sm:gap-20 sm:grid-cols-3 sm:justify-items-center"
+        class="grid grid-cols-1 gap-8 ml-20 mr-20 sm:gap-20 sm:grid-cols-3 sm:justify-items-center"
       >
-        <div>
-          <h2 class="mb-4 text-lg font-bold uppercase text-sky-700">
+        <div class="text-center">
+          <h2
+            class="mb-4 text-lg font-bold uppercase text-sky-700 lg:text-left"
+          >
             Services
           </h2>
-          <ul class="text-gray-600 dark:text-gray-400">
+          <ul class="text-center md:text-left text-gray-600 dark:text-gray-400">
             <li class="font-semibold text-sky-700">Oral Prophylaxis</li>
             <li class="font-semibold text-sky-700">Tooth Restoration</li>
             <li class="font-semibold text-sky-700">Whitening</li>
@@ -41,51 +45,65 @@
           </ul>
         </div>
 
-        <div>
-          <h2 class="mb-4 text-lg font-bold uppercase text-sky-700">
+        <div class="justify-center text-center">
+          <h2
+            class="mb-4 text-lg font-bold uppercase text-sky-700 lg:text-left"
+          >
             Quick Links
           </h2>
-          <ul class="text-gray-600">
+          <ul class="text-center md:text-left text-gray-600">
             <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
+              <a href="/" class="font-semibold hover:underline text-sky-700"
                 >Home</a
               >
             </li>
 
             <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
+              <a
+                href="/DentalServices"
+                class="font-semibold hover:underline text-sky-700"
                 >Dental Services</a
               >
             </li>
             <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
+              <a
+                href="/Appointments"
+                class="font-semibold hover:underline text-sky-700"
                 >Appointments</a
               >
             </li>
             <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
+              <a
+                href="/About"
+                class="font-semibold hover:underline text-sky-700"
                 >About Us</a
               >
             </li>
             <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
+              <a
+                href="/Contact"
+                class="font-semibold hover:underline text-sky-700"
                 >Contact Us</a
               >
             </li>
             <li>
-              <a href="#" class="font-semibold hover:underline text-sky-700"
+              <a
+                href="/accountsettings"
+                class="font-semibold hover:underline text-sky-700"
                 >My Account</a
               >
             </li>
           </ul>
         </div>
-        <div>
-          <h2 class="mb-4 text-lg font-bold uppercase text-sky-700">Extras</h2>
-          <ul class="text-gray-600 dark:text-gray-400">
+        <div class="text-center">
+          <h2
+            class="mb-4 text-lg font-bold uppercase text-sky-700 lg:text-left"
+          >
+            Extras
+          </h2>
+          <ul class="text-center md:text-left text-gray-600 dark:text-gray-400">
             <li>
-              <a
-                href="Faqs.html"
-                class="font-semibold hover:underline text-sky-700"
+              <a href="/faqs" class="font-semibold hover:underline text-sky-700"
                 >FAQs</a
               >
             </li>
@@ -95,11 +113,11 @@
     </div>
     <hr class="my-4 border-sky-700 sm:mx-auto dark:border-sky-700 lg:mt-10" />
 
-    <div class="p-2 sm:flex sm:items-center sm:justify-center">
+    <div class="p-2 flex items-center justify-center">
       <span
-        class="text-sm font-semibold text-gray-500 sm:text-center dark:text-gray-400"
+        class="text-sm font-semibold text-gray-500 text-center dark:text-gray-400"
         >© 2022
-        <a href="index.html" class="font-semibold hover:underline text-sky-700"
+        <a href="/" class="font-semibold hover:underline text-sky-700"
           >LFI Dental CLinic ™</a
         >. All Rights Reserved.
       </span>
