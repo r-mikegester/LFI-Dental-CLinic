181b7c8da3b23aeb8e22c5417c6977112c595f25
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 10:19:43 +0800

    requestProcedureAccess.js: add composable

---
+++ src/composables/api/access-procedure/requestProcedureAccess.js
@@ -0,0 +1,19 @@
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+
+const db = getFirestore();
+export default async (patientUid, slotSeconds) => {
+  const docRef = await doc(
+    db,
+    `users/${patientUid}/appointments/${slotSeconds}`
+  );
+
+  await setDoc(
+    docRef,
+    {
+      procedureVisible: "requesting",
+    },
+    {
+      merge: true,
+    }
+  );
+};
