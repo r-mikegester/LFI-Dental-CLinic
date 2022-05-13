1ee1d433826427616d6fce9f13ff5d8023ff3a48
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 10:08:17 +0800

    redirectToPatientLoginIfNotLoggedInUser.js: add route validation

---
+++ src/composables/route-validation/redirectToPatientLoginIfNotLoggedInUser.js
@@ -0,0 +1,13 @@
+import "../../firebase";
+import { getAuth } from "firebase/auth";
+
+const auth = getAuth();
+
+export default async () => {
+  const currentUser = auth.currentUser;
+  if (!currentUser) {
+    return {
+      name: "Patient Login Page",
+    };
+  }
+};
