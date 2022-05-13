5ad94045af2eae9d0128cf25f9f541011dbb42fd
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 10:27:29 +0800

    getDateTwoDaysBeforeTimeslot.js: add composable

---
+++ src/composables/calendar/getDateTwoDaysBeforeTimeslot.js
@@ -0,0 +1,27 @@
+import { DateTime } from "luxon";
+
+export default (slotSeconds) => {
+  const date = new Date(slotSeconds * 1000);
+  const month = date.toLocaleString("en-us", {
+    timeZone: "Asia/Manila",
+    month: "numeric",
+  });
+  const day = date.toLocaleString("en-us", {
+    timeZone: "Asia/Manila",
+    day: "numeric",
+  });
+  const year = date.toLocaleString("en-us", {
+    timeZone: "Asia/Manila",
+    year: "numeric",
+  });
+
+  const isoDateStr = DateTime.fromObject({
+    year: parseInt(year),
+    month: parseInt(month),
+    day: parseInt(day),
+  })
+    .minus({ days: 2 })
+    .toISO();
+
+  return new Date(isoDateStr);
+};
