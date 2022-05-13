a04106efb3c77e1d28a55088c984a96696a3bdcf
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-14 06:14:07 +0800

    deleteProfilePicture.js: add composable

---
+++ src/composables/account/deleteProfilePicture.js
@@ -0,0 +1,11 @@
+import { getAuth } from "@firebase/auth";
+import { getStorage, ref, deleteObject } from "firebase/storage";
+
+const storage = getStorage();
+const auth = getAuth();
+
+export default async () => {
+  const uid = auth.currentUser.uid;
+  const profilePictureRef = ref(storage, `profilePictures/${uid}`);
+  await deleteObject(profilePictureRef);
+};
