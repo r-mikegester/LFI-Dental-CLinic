ef07c35ab064fcd2f0c7ab9bb1e6c6f456f79daa
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 11:40:48 +0800

    PatientRecordsPage.vue: enable patients list

--- src/views/admin/patient-records/PatientRecordsPage.vue
+++ src/views/admin/patient-records/PatientRecordsPage.vue
@@ -1,75 +1,70 @@
 <script setup>
 import BaseLayout from "../../../components/admin//BaseLayout.vue";
-import { RouterLink } from "vue-router";
+import { computed, onMounted, ref } from "vue";
+import listUserProfiles from "../../../composables/api/listUserProfiles";
+import PatientRecordsPagePatientItem from "../../../components/admin/PatientRecordsPagePatientItem.vue";
+
+const usersList = ref([]);
+
+onMounted(async () => {
+  usersList.value = await listUserProfiles();
+});
+
+const patientsList = computed(() => {
+  return usersList.value.filter(
+    (user) => user.customClaims.accountType === "patient"
+  );
+});
+
+const nameFilter = ref("");
+
+const patientsListFilteredByName = computed(() => {
+  return patientsList.value.filter((user) => {
+    return user.displayName.toLowerCase().includes(nameFilter.value);
+  });
+});
 </script>
 
 <template>
   <BaseLayout>
-    <div>
-      <h1 class="text-3xl font-bold text-left p-2 pb-20">Patient Records</h1>
+    <div class="grid grid-cols-[auto_1fr] items-end mb-6">
+      <!-- Page title -->
+      <h1 class="text-2xl font-semibold mb-3">Patient Records</h1>
+      <!-- Search box -->
+      <div>
+        <label class="relative block float-right">
+          <span class="absolute inset-y-0 right-2 flex items-center pl-2">
+            <img src="../../../assets/img/search-icon.png" />
+          </span>
+          <input
+            class="w-full placeholder:normal placeholder:text-slate-400 block bg-white border-2 border-sky-400/60 border-3px py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            placeholder="Search names ..."
+            type="text"
+            name="search"
+            v-model="nameFilter"
+          />
+        </label>
+      </div>
     </div>
-    <div>
-      <label class="relative block float-right">
-        <span class="absolute inset-y-0 right-2 flex items-center pl-2">
-          <img src="../../../assets/img/search-icon.png" />
-        </span>
-        <input
-          class="placeholder:normal placeholder:text-slate-400 block bg-white w-80 border-2 border-sky-400/60 border-3px py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-          placeholder="Search Name"
-          type="text"
-          name="search"
-        />
-      </label>
-    </div>
-    <div class="mt-12 bg-teal-500/40">
-      <div class="p-4 grid grid-cols-[1fr_2fr_2fr_2fr_2fr_1fr] font-semibold">
-        <div>Patient ID</div>
+    <div class="bg-teal-500/40">
+      <div
+        class="p-4 gap-4 grid grid-cols-[minmax(0,_1fr)_minmax(0,_2fr)_repeat(3,_minmax(0,_1fr))] font-semibold"
+      >
         <div>Name</div>
+        <div>Email</div>
         <div>Medical Chart</div>
         <div>Dental Chart</div>
         <div>Dental Treatment</div>
       </div>
     </div>
-    <div class="text-left">
-      <div
-        class="border-b-[1px] border-teal-500 p-4 grid grid-cols-[1fr_2fr_2fr_2fr_2fr_1fr]"
-      >
-        <div>0019</div>
-        <div>Jane Doe</div>
-        <div class="hover:text-sky-500"><button>View</button></div>
-        <div class="hover:text-sky-500"><button>View</button></div>
-        <div class="hover:text-sky-500">
-          <RouterLink
-            :to="{ name: 'Admin Dental Treatments Page', params: { uid: 123 } }"
-            >View
-          </RouterLink>
-        </div>
-        <div>
-          <button class="mr-5 float-right">
-            <img src="../../../assets/img/trash-icon.png" />
-          </button>
-        </div>
-      </div>
-      <div
-        class="border-b-[1px] border-teal-500 p-4 grid grid-cols-[1fr_2fr_2fr_2fr_2fr_1fr]"
-      >
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div
-        class="border-b-[1px] border-teal-500 p-4 grid grid-cols-[1fr_2fr_2fr_2fr_2fr_1fr]"
-      >
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+    <div v-if="patientsListFilteredByName.length > 0">
+      <PatientRecordsPagePatientItem
+        v-for="user in patientsListFilteredByName"
+        :key="user.uid"
+        :uid="user.uid"
+        :email="user.email"
+        :fullName="user.displayName"
+      />
     </div>
   </BaseLayout>
 </template>
