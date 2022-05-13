f70ccf1ea367c0dd8ab5cc11d2d0d68191c8f31f
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 10:18:43 +0800

    setUserAppointmentProcedure.js: add composable

---
+++ src/composables/api/procedures/setUserAppointmentProcedure.js
@@ -0,0 +1,27 @@
+import getUserToken from "../../auth/getUserToken";
+import backendBaseURL from "../backendBaseURL";
+import HttpError from "../../helpers/HttpError";
+import ParameterError from "../../helpers/ParameterError";
+
+export default async (patientUid, slotSeconds, procedureBody) => {
+  if (!patientUid) throw new ParameterError("patientUid");
+  if (!slotSeconds) throw new ParameterError("slotSeconds");
+  if (!procedureBody) throw new ParameterError("procedureBody");
+
+  const idToken = await getUserToken();
+  const link = `${backendBaseURL}/users/${patientUid}/appointments/${slotSeconds}/procedure`;
+  const response = await fetch(link, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${idToken}`,
+    },
+    body: JSON.stringify({
+      procedureBody,
+    }),
+  });
+  const data = await response.json();
+
+  if (!response.ok) throw new HttpError(response.status, data.message);
+  return data.payload;
+};
