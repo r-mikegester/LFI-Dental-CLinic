e397fc8fd63e83751e9dbec5c02acfad1edb5878
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-16 00:52:32 +0800

    setPatientSignature.js: add composable

---
+++ src/composables/firestore/signature-fields/setPatientSignature.js
@@ -0,0 +1,10 @@
+import { doc, getFirestore, setDoc } from "@firebase/firestore";
+
+const db = getFirestore();
+
+export default async (patientUid, dataUrl) => {
+  const docRef = doc(db, `users/${patientUid}/patientRecords/patientSignature`);
+  await setDoc(docRef, {
+    dataUrl,
+  });
+};
