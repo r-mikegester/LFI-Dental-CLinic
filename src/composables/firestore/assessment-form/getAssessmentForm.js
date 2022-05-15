909acb214328fdf1f17819ea026f433f76fed786
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-16 00:53:56 +0800

    getAssessmentForm.js: add composable

---
+++ src/composables/firestore/assessment-form/getAssessmentForm.js
@@ -0,0 +1,11 @@
+import { doc, getDoc, getFirestore } from "@firebase/firestore";
+
+const db = getFirestore();
+
+export default async (patientUid) => {
+  const docRef = doc(db, `users/${patientUid}/patientRecords/assessmentForm`);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists) return docSnap.data();
+  else return {};
+};
