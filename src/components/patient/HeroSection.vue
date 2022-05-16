11a512109e5daf62557eaa65874aaed2d4063e2b
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-16 19:43:34 +0800

    HeroSection.vue: add text-shadow to hero section text

--- src/components/patient/HeroSection.vue
+++ src/components/patient/HeroSection.vue
@@ -7,6 +7,7 @@
     />
     <div
       class="absolute top-0 left-0 flex items-center justify-center w-full h-full text-4xl font-bold text-center text-white uppercase"
+      style="text-shadow: rgba(0, 0, 0, 0.5) 1px 0 6px"
     >
       <slot></slot>
     </div>
