11640e57f920bc3e34260aa0318a186616c6f0d2
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-21 01:42:50 +0800

    setFilledInMedicalChart.js: add composable

---
+++ src/composables/firestore/setFilledInMedicalChart.js
@@ -0,0 +1,16 @@
+import { doc, setDoc, getFirestore } from "@firebase/firestore";
+import "../../firebase";
+const db = getFirestore();
+
+export default async (patientUid) => {
+  const docRef = doc(db, `users/${patientUid}`);
+  await setDoc(
+    docRef,
+    {
+      filledInMedicalChart: true,
+    },
+    {
+      merge: true,
+    }
+  );
+};
