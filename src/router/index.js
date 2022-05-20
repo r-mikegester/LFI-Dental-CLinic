fdd37682ad22e59d972231b0a12ea1b0f0e793f0
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-20 20:42:48 +0800

    router: add scroll behavior
    This allows RouterLinks with hash to work

--- src/router/index.js
+++ src/router/index.js
@@ -282,6 +282,15 @@ const router = createRouter({
       },
     },
   ],
+  scrollBehavior(to) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+      };
+    }
+    // always scroll to top
+    return { top: 0 };
+  },
 });
 
 export default router;
