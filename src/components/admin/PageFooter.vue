ebcb2e294e0452b1260bfa1c2eae69415dce77d6
Mike Gester <mikegester.sabuga023@gmail.com>
2022-11-27 14:20:59 +0800

    All elemets merge with the FooterSection

--- src/components/admin/PageFooter.vue
+++ src/components/admin/PageFooter.vue
@@ -1,5 +1,5 @@
+// eslint-disable-next-line vue/valid-template-root
 <template>
-  <footer class="bg-teal-500 text-white text-center text-lg py-4 font-medium">
-    LFI Dental Clinic &copy; 2022
-  </footer>
+  <!--ALL FOOTER elemets was now move in FooterSection.vue-->
+  <div></div>
 </template>
