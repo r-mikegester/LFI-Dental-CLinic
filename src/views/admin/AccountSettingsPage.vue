9484808bcdaf1dc6cf7d8f837149cfcd41cca3d7
Mike Gester <mikegester.sabuga023@gmail.com>
2022-12-02 02:13:58 +0800

    Add Logout btn for mobile view 4 easier navigation

--- src/views/admin/AccountSettingsPage.vue
+++ src/views/admin/AccountSettingsPage.vue
@@ -5,5 +5,11 @@ import AccountSettings from "../../components/shared/AccountSettings.vue"
 <template>
   <BaseLayout>
     <AccountSettings />
+    <button
+      class="flex items-center px-8 py-3 mx-auto border-2 rounded-lg border-teal-500 transition duration-200 hover:bg-teal-500/40 lg:mt-auto md:w-20 lg:w-64 lg:hidden md:hidden"
+      @click="onLogout()">
+      <img src="../../assets/img/nav-exit.png" class="mr-5 md:block" />
+      <span class="md:hidden lg:block items-center">Log out</span>
+    </button>
   </BaseLayout>
 </template>
