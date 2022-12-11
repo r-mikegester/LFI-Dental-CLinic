889e2d63843a9826b1b13aa6a24dbbbba4990eeb
Angelo Geulin <angelogeulin123@gmail.com>
2022-12-11 19:37:36 +0800

    fix: clear selected appointment details when leaving appointment scheduling flow

--- src/views/AppointmentsPage.vue
+++ src/views/AppointmentsPage.vue
@@ -2,6 +2,13 @@
 import BaseLayout from "../components/patient/BaseLayout.vue"
 import HeroSection from "../components/patient/HeroSection.vue"
 import { RouterView } from "vue-router"
+import { onUnmounted } from "vue"
+import { useAppointmentDetailsStore } from "../stores/appointmentDetails"
+
+const appointmentDetailsStore = useAppointmentDetailsStore()
+onUnmounted(() => {
+  appointmentDetailsStore.$reset()
+})
 </script>
 
 <template>
