8fc9ba4eff80637ea1ff7ea16ae4af039f01c125
Angelo Geulin <angelogeulin123@gmail.com>
2023-01-11 13:41:38 +0800

    chore: remove unused error class

--- src/composables/helpers/DatabaseError.js
+++
@@ -1,6 +0,0 @@
-export default class FirestoreError extends Error {
-  constructor(taskName, errorMessage) {
-    const message = `Error while ${taskName}: ${errorMessage}`
-    super(message)
-  }
-}
